c450d04aa111a1f7ffb3c64dfc231c973eb8a2c8
Air1K <veremeenkom91@gmail.com>
2022-12-21 00:58:14 +0500

    Метод дейкстры полностью готовый + меню 1

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {Context} from "./index";
 import {BrowserRouter, Routes, Route,  Navigate} from "react-router-dom";
 import Authorization from "./component/autorization/autorization";
 import VvodPlana from "./component/vvodPlana";
+import NewComponentMain from "./component/newComponentMain";
 
 function App() {
     const {store} = useContext(Context);
@@ -13,14 +14,15 @@ function App() {
     })
     return (
         <div className={styles.app}>
-
             <BrowserRouter>
                 <Routes>
+                    <Route path="" element={<NewComponentMain/>}/>
                     <Route path="authorization" element={<Authorization/>}/>
                     <Route path="main" element={<Main/>}/>
                     <Route path="main/plan" element={<VvodPlana/>}/>
                 </Routes>
             </BrowserRouter>
+
         </div>
     );
 }
---
+++ src/component/newComponentMain.tsx
@@ -0,0 +1,14 @@
+import React, {useContext} from 'react';
+import {Navigate} from "react-router-dom";
+import {Context} from "../index";
+
+const NewComponentMain = () => {
+    const {store} = useContext(Context);
+    return (
+        <div>
+            {store.isAuth ? (<Navigate to={'/main'} />):(<Navigate to={'/authorization'} />)}
+        </div>
+    );
+};
+
+export default NewComponentMain;
